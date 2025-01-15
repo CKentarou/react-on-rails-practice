d98d85c4e42d7cfea907a0d49eec487098d6a2f9
CKentarou <k023c0018@g.neec.ac.jp>
2025-01-15 16:41:01 +0900

    CORS設定とRails-React連携

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+
+  useEffect(() => {
+    fetch('http://localhost:3000/books')
+      .then(response => response.json())
+      .then(data => setBooks(data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Books</h1>
+      <ul>
+        {books.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
